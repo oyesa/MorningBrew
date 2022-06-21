73c187723fcdf7620e59c9c105fdbb65c73063a7
oyesa <mercyoyesa2018@gmail.com>
2022-06-21 15:50:58 +0300

    create fnduser in models

--- auth/models.py
+++ auth/models.py
@@ -29,3 +29,21 @@ class FndUserManager(BaseUserManager):
       user.save()
 
       return user
+
+
+class FndUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(('first name'), max_length=30, blank=True)
+  
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = FndUserManager()
+
+    def __str__(self):
+        return self.email
