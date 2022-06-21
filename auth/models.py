88c3f11876a551425f51cca6f62d7a81ff00226f
oyesa <mercyoyesa2018@gmail.com>
2022-06-21 15:43:54 +0300

    create fndusermanager model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
 # Create your models here.
+class FndUserManager(BaseUserManager):
+    def create_user(self, username, email, first_name, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        fndUser = self.model(username=username, email=self.normalize_email(email), first_name=first_name)
+        fndUser.set_password(password)
+        fndUser.save()
+
+        return fndUser
+
+    def create_superuser(self, username, email, password):
+      if password is None:
+          raise TypeError('Superusers must have a password.')
+
+      user = self.create_user(username, email, password)
+      user.is_superuser = True
+      user.is_staff = True
+      user.is_verified=True
+      user.save()
+
+      return user
--- fndproject/settings.py
+++ fndproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'fndproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fnd',
+        'USER': 'oyesa',
+    'PASSWORD':'Mimo33',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'authapp.FndUser'
