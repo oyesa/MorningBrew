f5ee4b23ea827d47e9e198b5b14befa52082fcec
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-22 12:39:43 +0300

    configure database settings

--- fndproject/settings.py
+++ fndproject/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'fndproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'fnd',
+        'USER': 'postgres',
+        'HOST':'localhost',
+        'PASSWORD': 'SEMBERUA',
+        'PORT': '5432',
     }
 }
 
