f6d5a0563aea86aee694adae0d1bc81c29fd428b
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-22 13:25:53 +0300

    configure admin GeoLocation function

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Incidences
+from leaflet.admin import LeafletGeoAdmin
 
 
 #ADMIN=MorningBrew 
 #Password=SEMBERUA
 # email:kevin.kipkemoi@student.moringaschool.com 
 # Register your models here.
-class IncidencesAdmin(admin.ModelAdmin):
+class IncidencesAdmin(LeafletGeoAdmin):
     list_display=('name', 'location')
 
 
