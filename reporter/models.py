240e42781097743eab47673b485834f84dcda819
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-22 13:27:02 +0300

    configure GeoAdmin class and function leaft

--- reporter/models.py
+++ reporter/models.py
@@ -1,11 +1,13 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 
 class Incidences(models.Model):
   name = models.CharField(max_length=20)
-  location= models.CharField(max_length=20)
+  location= models.PointField(srid=4326)
+  objects= models.GeoManager()
 
   def __unicode__(self):
     return self.name
