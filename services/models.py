dc7bc2f6e7beae4e3ef76fee87678c5f874fb953
rachel <rachelkemuma99@gmail.com>
2022-06-22 14:09:18 +0300

    create model service

--- services/models.py
+++ services/models.py
@@ -1,4 +1,12 @@
+from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
 
+    def save_service(self):
+        self.save()
