597b35aadfaa44a131e901bce8aa7a9824d6a85f
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-22 17:00:50 +0300

    creating methods of the model

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,3 +5,22 @@ class Portfolio(models.Model):
     artisan = models.CharField(max_length=200, blank=True,null=True)
     phone_no = models.IntegerField(blank=True)
     description = models.TextField(blank=True, max_length=300)
+
+    def __str__(self):
+        return self.artisan
+
+    def create_portfolio(self):
+        """
+        A method that creates a portfolio
+        """
+        self.save()
+
+    @classmethod
+    def find_portfolio(cls, portfolio_id):
+        """
+        A method that finds a neighbourhood using its id
+        """
+
+         
+        portfolios=Portfolio.objects.filter(id=portfolio_id) 
+        return portfolios
