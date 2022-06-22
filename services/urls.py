48408e04c440ccd0d9c6cbb6bfc756ccbd89b643
rachel <rachelkemuma99@gmail.com>
2022-06-22 15:38:52 +0300

    create a permissions file

--- services/urls.py
+++ services/urls.py
@@ -1,10 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from .views import *
 from django.urls import path,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-router = DefaultRouter()
-router.register('service',ServiceViewSet)
+# router = DefaultRouter()
+# router.register('service',ServiceViewSet)
 
-urlpatterns=[
-    path('add/',include(router.urls))
+urlpatterns = [
+    path('api/service/',views.ServiceList.as_view()),
+    # path('add/',include(router.urls))
 ]
--- services/views.py
+++ services/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializer import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ServiceSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
-class ServiceViewSet(ModelViewSet):
-    serializer_class=ServiceSerializer
-    queryset=Service.objects.all()
+# class ServiceViewSet(ModelViewSet):
+#     serializer_class=ServiceSerializer
+#     queryset=Service.objects.all()
+class ServiceList(APIView):
+    def get(self, request, format=None):
+        all_merch = Service.objects.all()
+        serializers = ServiceSerializer(all_merch, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = ServiceSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (IsAdminOrReadOnly,)
 
