854fcb96451979d2b699d64417a53aa18012c951
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-22 12:32:00 +0300

    create class for Incidents: name and location 	# Please enter the commit message for your changes. Lines starting

--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,14 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Incidences(models.Model):
+  name = models.CharField(max_length=20)
+  location= models.CharField(max_length=20)
+
+  def __unicode__(self):
+    return self.name
+   
+  class Meta:
+    verbose_name_plural="Incidences"
