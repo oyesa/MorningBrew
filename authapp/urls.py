b520a52f66c0911357d950873971ca2b3a101b81
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-22 09:19:32 +0300

    adding paths for view functions

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,6 +1,9 @@
-from .views import RegistrationAPIView
+from .views import *
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('register', RegistrationAPIView.as_view(), name='AuthRegistry')
+    path('register', RegistrationAPIView.as_view(), name='AuthRegistry'),
+    path('login', LoginApiView.as_view(), name='login'), 
+    path('user', FndUserRetrieveupdateApiView.as_view(),name='updateUser')
+
 ]
