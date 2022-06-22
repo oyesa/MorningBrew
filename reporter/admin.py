628de77cdaeb3ee2e03e6ece85f4ad4b06f8d69b
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-22 12:31:13 +0300

    configure: admin login authenication

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Incidences
 
+
+#ADMIN=MorningBrew 
+#Password=SEMBERUA
+# email:kevin.kipkemoi@student.moringaschool.com 
 # Register your models here.
+class IncidencesAdmin(admin.ModelAdmin):
+    list_display=('name', 'location')
+
+
+admin.site.register(Incidences, IncidencesAdmin)
