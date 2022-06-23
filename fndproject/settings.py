b3e62680faa03138839a73db9268755b60c951c3
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-23 13:55:43 +0300

    project settings modify

--- fndproject/settings.py
+++ fndproject/settings.py
@@ -147,3 +147,8 @@ LEAFLET_CONFIG ={
     'SCALE': 'both', #imperial/metric
     'ATTRIBUTION_PREFIX':'Inspired By Life in Moringa School'
 }
+
+if os.name == 'nt':
+    VENV_BASE = os.environ['VIRTUAL_ENV']
+    os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
