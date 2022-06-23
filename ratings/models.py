d9e32e2208b2f29701982059b9ae4059bcf509a9
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-23 10:04:38 +0300

    creating methods in class ratings

--- ratings/models.py
+++ ratings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Rating(models.Model):
+class Ratings(models.Model):
     ratings=(
         (1, '1'),
         (2, '2'),
@@ -14,3 +14,23 @@ class Rating(models.Model):
         (9, '9'),
         (10, '10',)
     )
+    # service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='ratings', null=True)
+    skills = models.IntegerField(choices=ratings, default=0, blank=True)
+    time = models.IntegerField(choices=ratings, blank=True, default=0)
+    affordability = models.IntegerField(choices=ratings, blank=True,default=0)
+    overall_score = models.FloatField(default=0, blank=True)
+    skills_avr = models.FloatField(default=0, blank=True)
+    time_avr = models.FloatField(default=0, blank=True)
+    affordability_avr = models.FloatField(default=0, blank=True)
+
+    def save_ratings(self):
+        self.save()
+
+    def __str__(self):
+        return f'{self.post} Ratings'
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Ratings.objects.filter(service_id=id).all()
+        return ratings
+
