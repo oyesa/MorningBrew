3add1c2d551d5b7ee6a8d23d0dff3ee6fb25ebdd
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-23 17:51:33 +0300

    ft: template modification

--- reporter/views.py
+++ reporter/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.http import HttpResponse
-from models import Counties,Incidences
-from reporter import views
+from .models import Counties,Incidences
+
 
 
 
